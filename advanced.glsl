b9b03c5888fd9ebbb3b14c58f4d3756e396af7e0
Tyler Lindberg <tlindberg@ucla.edu>
2018-08-01 20:06:27 -0700

    Modified uv scaling for advanced vignette

--- advanced.glsl
+++ advanced.glsl
@@ -9,10 +9,10 @@ float vignette(vec2 uv, vec2 size, float roundness, float smoothness) {
 	// Center UVs
 	uv -= 0.5;
 
-	// Rescale UVs to be uniform
+	// Shift UVs based on the larger of width or height
 	float minWidth = min(size.x, size.y);
-	uv.x *= minWidth / size.x;
-	uv.y *= minWidth / size.y;
+	uv.x = sign(uv.x) * clamp(abs(uv.x) - abs(minWidth - size.x), 0.0, 1.0);
+	uv.y = sign(uv.y) * clamp(abs(uv.y) - abs(minWidth - size.y), 0.0, 1.0);
 
 	// Signed distance calculation
 	float boxSize = minWidth * (1.0 - roundness);
